b0d6b8d94b0bf3576fdeb9c586eb974f02b52798
Deep <bahttacharyyadeep2002@gmail.com>
2024-05-20 23:01:11 +0530

    changes

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl=process.env.NODE_ENV==="production"?"server.com":
+export const baseUrl=process.env.NODE_ENV==="production"?process.env.SERVER_BASE_URL:
 "http://localhost:3000/";
